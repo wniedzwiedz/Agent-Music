534fb05178d4f15ec2b1bfa8a15bc23ac92a2abe
Kacper Zybała <zyperpl@gmail.com>
2022-01-08 00:34:13 +0100

    Add Fluidsynth library

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from PyQt6.Qt6 import *
 
 import sys, time, random
 
+from external import fluidsynth
 from canvas import *
 
 
@@ -54,5 +55,28 @@ class MainWindow(QWidget):
 app = QApplication(sys.argv)
 window = MainWindow()
 window.show()
+
+testFluidsynth = False
+
+if testFluidsynth:
+    fs = fluidsynth.Synth()
+    fs.start()
+
+    sfid = fs.sfload("example.sf2")
+    fs.program_select(0, sfid, 0, 0)
+
+    fs.noteon(0, 60, 30)
+    fs.noteon(0, 67, 30)
+    fs.noteon(0, 76, 30)
+
+    time.sleep(1.0)
+
+    fs.noteoff(0, 60)
+    fs.noteoff(0, 67)
+    fs.noteoff(0, 76)
+
+    time.sleep(1.0)
+
+    fs.delete()
 
 app.exec()
